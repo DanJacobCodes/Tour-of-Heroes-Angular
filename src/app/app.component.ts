e0588d01e0dc654d99b41e9ad02a28fe8db936bd
Dan Jacob <dansamueljacob@gmail.com>
2017-08-05 16:58:53 -0700

    make heroes of type HEROES array in AppComponent, use ngFor directive to display all heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,39 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  {id: 11, name:'Spider-Man'},
+  {id: 12, name:'Batman'},
+  {id: 13, name:'Hulk'},
+  {id: 14, name:'Iron Man'},
+  {id: 15, name:'Storm'},
+  {id: 16, name:'Flash'},
+  {id: 17, name:'Nightcrawler'}
+];
+
 @Component({
   selector: 'my-app',
   template: `
-  <h1>{{title}}</h1>
-  <h2>{{hero.name}} details</h2>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name">
-  </div>
-
-  `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    <h2>My Heroes</h2>
+<ul class="heroes">
+<li *ngFor="let hero of heroes">
+<span class="badge">{{hero.id}}</span> {{hero.name}}
+</li>
+</ul>
+    `
 })
-export class AppComponent  {
-  title = "Tour of Heroes";
+export class AppComponent {
+  heroes = HEROES;
+  title = 'Tour of Heroes';
   hero: Hero = {
-    id: 1;
-    name: "Storm"
-  }
- }
+    id: 1,
+    name: 'Wolverine',
+  };
+}
