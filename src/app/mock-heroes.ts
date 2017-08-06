6860a8eb45a330a904d6c8efc4f3ad83c4943d60
Dan Jacob <dansamueljacob@gmail.com>
2017-08-06 12:33:24 -0700

    add hero service, remove array of hardcoded heroes into a mock hero file

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+const HEROES: Hero[] = [
+  {id: 11, name:'Spider-Man'},
+  {id: 12, name:'Batman'},
+  {id: 13, name:'Hulk'},
+  {id: 14, name:'Iron Man'},
+  {id: 15, name:'Storm'},
+  {id: 16, name:'Flash'},
+  {id: 17, name:'Nightcrawler'}
+];
